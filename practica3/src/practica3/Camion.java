f686b041518f9f341fb3cc615de0877ef1566bab
Diego Alfonso Candelaria Rodríguez <dcandelaria@users.noreply.github.com>
2018-12-10 11:19:36 +0100

    cosas

--- practica3/src/practica3/Camion.java
+++ practica3/src/practica3/Camion.java
@@ -5,5 +5,15 @@ package practica3;
  * @author 
  */
 public class Camion extends Vehiculo {
-    
+	public Camion(){
+		combustible=new Combustible(4);
+		radar=new Radar(11);
+		gps=new GPS();
+		escaner=new Escaner();
+	}
+	
+	@Override
+	public Direccion llegarMeta(){
+		return Direccion.N;
+	}
 }
--- practica3/src/practica3/Vehiculo.java
+++ practica3/src/practica3/Vehiculo.java
@@ -5,5 +5,14 @@ package practica3;
  * @author 
  */
 public abstract class Vehiculo {
-    
+	protected Radar radar;
+	protected Combustible combustible;
+	protected GPS gps;
+	protected Escaner escaner;
+	
+	public abstract Direccion llegarMeta();
+	
+	public void consumirCombustible(){
+		combustible.consumir();
+	}
 }
