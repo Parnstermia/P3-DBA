fb000d8546fc551fcf8df824f13fa0e6fba9ecee
Diego Alfonso Candelaria Rodríguez <dcandelaria@users.noreply.github.com>
2018-12-17 02:20:40 +0100

    Modificada la estructura de los vehículos (para que no se repita código) y hecho el algortimo de llegada a la meta para el vehículo volador.

--- practica3/src/practica3/Camion.java
+++ practica3/src/practica3/Camion.java
@@ -4,16 +4,11 @@ package practica3;
  *
  * @author 
  */
-public class Camion extends Vehiculo {
+public class Camion extends VehiculoTierra {
 	public Camion(){
 		combustible=new Combustible(4);
 		radar=new Radar(11);
 		gps=new GPS();
 		escaner=new Escaner();
-	}
-	
-	@Override
-	public Direccion llegarMeta(){
-		return Direccion.N;
 	}
 }
---
+++ practica3/src/practica3/VehiculoTierra.java
@@ -0,0 +1,20 @@
+package practica3;
+
+/**
+ *
+ * @author 
+ */
+public abstract class VehiculoTierra {
+	protected Radar radar;
+	protected Combustible combustible;
+	protected GPS gps;
+	protected Escaner escaner;
+	
+	public Direccion llegarMeta(){
+		Direccion direccion=Direccion.N;
+		
+		
+		
+		return direccion;
+	}
+}
